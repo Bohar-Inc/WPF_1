8a271ea09ceda3f3f01eca6fbb89734b1d4e0d01
Kopikanth <118206393+Kopikanth@users.noreply.github.com>
2024-11-25 15:54:33 +0530

    navigation

--- Wpf_1/App.xaml.cs
+++ Wpf_1/App.xaml.cs
@@ -11,16 +11,19 @@ namespace Wpf_1
     /// </summary>
     public partial class App : Application
     {
+        private readonly ModalNavigationStore _modalNavigationStore;
         private readonly SelectedYouTubeViewerStore _selectedYouTubeViewerStore;
         public App()
         {
+            _modalNavigationStore = new ModalNavigationStore();
             _selectedYouTubeViewerStore = new SelectedYouTubeViewerStore();
         }
         protected override void OnStartup(StartupEventArgs e)
         {
+            YouTubeViewersViewModel youTubeViewersViewModel = new YouTubeViewersViewModel(_selectedYouTubeViewerStore);
             MainWindow = new MainWindow()
             {
-                DataContext = new YouTubeViewersViewModel(_selectedYouTubeViewerStore)
+                DataContext = new MainViewModel(_modalNavigationStore, youTubeViewersViewModel)
             };
             MainWindow.Show();
             base.OnStartup(e);
--- Wpf_1/ViewModels/MainViewModel.cs
+++ Wpf_1/ViewModels/MainViewModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wpf_1.Stores;
 
 namespace Wpf_1.ViewModels
 {
-    class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        private readonly ModalNavigationStore _modalNavigationStore;
+        public ViewModelBase CurrentModalViewModal => _modalNavigationStore.CurrentViewModel;
+        public bool IsModalOpen => _modalNavigationStore.IsOpen;
+        public YouTubeViewersViewModel YouTubeViewersViewModel { get; }
+        public MainViewModel(ModalNavigationStore modalNavigationStore, YouTubeViewersViewModel youTubeViewersViewModel)
+        {
+            _modalNavigationStore = modalNavigationStore;
+            YouTubeViewersViewModel = youTubeViewersViewModel;
+            _modalNavigationStore.CurrentViewModelChanged += ModalNavigationStore_CurrentViewModelChanged; 
+            _modalNavigationStore.CurrentViewModel = new AddYouTubeViewerViewModel();
+        }
+        protected override void Dispose()
+        {
+            _modalNavigationStore.CurrentViewModelChanged -= ModalNavigationStore_CurrentViewModelChanged;
+            base.Dispose();
+        }
+        private void ModalNavigationStore_CurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentModalViewModal));
+            OnPropertyChanged(nameof(IsModalOpen));
+        }
     }
 }
